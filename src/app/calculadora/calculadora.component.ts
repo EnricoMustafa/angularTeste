c8a49a75665c73cbdb4df82e57a17064721a8f56
EnricoMustafa <enricobrasil123@gmail.com>
2025-08-10 22:20:56 -0300

    calculadora

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,33 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.scss'
+  styleUrl: './calculadora.component.scss',
 })
 export class CalculadoraComponent {
-
   numero1: number = 0;
   numero2: number = 0;
   resultado: number = 0;
 
-  somar(){
-    console.log("Chamando o metodo para calcular o resultado");
+  calcularResultado() {
+    console.log('Chamando o metodo para calcular o resultado');
     this.resultado = this.numero1 + this.numero2;
-  }
-
-  subtrair(){
-    this.resultado = this.numero1 - this.numero2;
-  }
-
-  multiplicar(){
-    this.resultado = this.numero1 * this.numero2;
-  }
-  
-  dividir(){
-    this.resultado = this.numero1 / this.numero2;
-    return this.resultado.toFixed(2)
   }
 }
